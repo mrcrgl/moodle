9938619dabdc38ebf12e364513b3af771c03c4bb
Marc Riegel <marc.riegel@net-m.de>
2013-03-11 14:04:22 +0100

    Added boolean type

---
+++ attr/bool.js
@@ -0,0 +1,31 @@
+/**
+ * @author: Marc Riegel <mail@marclab.de>
+ * Date: 11.03.13
+ * Time: 09:10
+ *
+ */
+
+var DefaultAttrType = require('./default');
+
+/**
+ * @class AttrBool
+ *
+ * @extends AttrDefault
+ * @param attr
+ * @constructor
+ */
+var AttrType = function(attr) {
+
+    this.initialize(attr);
+
+    this.validator(function(self, v) {
+        if (v !== true && v !== false) {
+            self.errors.push("Not a valid boolean: "+v);
+        }
+    });
+
+};
+
+AttrType.prototype = new DefaultAttrType();
+
+module.exports = AttrType;
