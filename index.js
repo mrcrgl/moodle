9f33832bf6e5abdccb80af065326bf33bff4d28c
Marc Riegel <marc.riegel@net-m.de>
2013-03-18 15:24:07 +0100

    Bigfix

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ module.exports = function(name) {
         this.prototype[sAttr] = function(val) {
             if (0 == arguments.length) return this.attrs[sAttr] || undefined;
 
+            val = model._attributes[sAttr].set(val);
+
             if (model._attributes[sAttr].validate(val)) {
                 //log.info("Attr %s is valid (%s)", sAttr, val);
             }
@@ -79,7 +81,10 @@ module.exports = function(name) {
     Model.findOne = function(filter, callback) {
         this._storageEngine.query().select().where(filter).limit(1).call(function(err, data) {
             if (data instanceof Array && data.length > 0) {
-                callback(err, new Model(data.shift()));
+                //var currentData = data.shift();
+                console.log("Create Model of " + JSON.stringify(data[0]));
+                var model = new Model(data[0]);
+                callback(err, model);
             } else {
                 callback(err);
             }
@@ -92,13 +97,17 @@ module.exports = function(name) {
         return this.model._attributes[attr];
     };
 
-
     Model.prototype.set = function(object) {
+        console.log("model.set called: " + object);
         if (object instanceof Object) {
+            console.log("object is instanceif Object");
             for (var attr in object) {
                 if (typeof this[attr] == 'function')
                     this[attr](object[attr]);
             }
+            console.dir(this.attrs);
+        } else {
+            console.error("Cannot set. Not a object: " + object);
         }
     };
 
@@ -129,12 +138,18 @@ module.exports = function(name) {
         if (check === true) {
 
             if (this.isValid()) {
+                console.log("doing update");
                 var where = {};
                 where[this.primaryKey] = this.attrs[this.primaryKey];
 
-                this.model._storageEngine.query().update().where(where).set(this.attrs).limit(1).call(callback);
+                this.model._storageEngine.query().update().where(where).set(this.attrs).limit(1).call(function(err) {
+                    console.log("got update callback");
+                    callback(err);
+                });
             } else {
+                console.log("doing insert");
                 this.model._storageEngine.query().insert().set(this.attrs).call(function(err, data) {
+                    console.log("got insert callback");
                     if (data.affectedRows > 0) {
                         this[this.primaryKey](data.insertId);
                     }
@@ -144,12 +159,24 @@ module.exports = function(name) {
             }
 
         } else {
+            console.dir("Save failed by validation: " + check);
             callback(check);
         }
     };
 
     Model.prototype.isValid = function() {
-        return !isNaN(this.attrs[this.primaryKey])
+        var id = this[this.primaryKey]();
+
+        if (!id) return false;
+
+        if (parseInt(id) === id) {
+            return true;
+        }
+
+        if ((new RegExp(/^[0-9a-f]{24}$/)).test(id)) {
+            return true;
+        }
+        return false;
     };
 
     Model.prototype.delete = function() {
